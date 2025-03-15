50612b6a8025176dc9860dcca678966f87c40280
johneyder <john.kazz@gmail.com>
2025-03-15 10:33:15 -0500

    fix

--- src/routes/test-page/+page.svelte
+++ src/routes/test-page/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { get } from 'svelte/store'
-	import { quotation, addItem, getQuotation } from '$lib/stores/shared'
+	import { quotation } from '$lib/stores/shared'
 
 	// const usb = quotation.subscribe((value) => {
 	// 	quo = value
@@ -8,6 +8,5 @@
 </script>
 
 <h1>Test Page</h1>
-<button class="btn" onclick={addItem}>update quotation</button>
 
 {JSON.stringify($quotation, null, 2)}
